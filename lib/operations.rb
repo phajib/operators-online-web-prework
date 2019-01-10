320c46487b300b69f76ebda40e2c6ffdb4ff0b0c
H B <phajib@gmail.com>
2019-01-10 09:54:33 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,6 +7,9 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-	speed > 60 || speed < 40 ? true : speed > 40 && speed < 60
+	if speed > 60 || speed < 40
+		true
+	elsif speed > 40 && speed < 60
 		false
+	end
 end
